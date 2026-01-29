bea0b5f15819a3a1a719d1e2d14b1e1a7bc64277
jeremymcwilliams <jeremym@lclark.edu>
2026-01-29 12:06:08 -0800

    Update 02-grant-privileges.sql

--- sql/02-grant-privileges.sql
+++ sql/02-grant-privileges.sql
@@ -1,3 +1,3 @@
 -- Grant the user ability to create databases
-GRANT CREATE, DROP ON *.* TO 'fakeAirbnbUser'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'fakeAirbnbUser'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
